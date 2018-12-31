9b9fcea9ba77deafb8bc797cfcaecdee688a27bf
Cillian Wing <cwing@zagmail.gonzaga.edu>
2018-12-31 01:01:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   new_array = []
   array.each do |sub_array|
-    square_number = sub_array[0] ** 2
+    square_number = sub_array ** 2
     new_array << square_number
   end
 end
